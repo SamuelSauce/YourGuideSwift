47d5e88c8583fc7e241e37de28112a6784d0f927
Samuel Bridge <samuelfbridge@Samuels-MacBook-Pro.local>
2018-06-30 16:37:03 -0600

    first commit

--- YourGuideSwift/SecondViewController.swift
+++ YourGuideSwift/SecondViewController.swift
@@ -14,7 +14,9 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var BackgroundImage: UIImageView!
+    @IBOutlet weak var ProfileImage: UIImageView!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
